96d923199a3a8787a4fd4aa54d05a6ea85ab16cb
fakefred <fakefred@protonmail.ch>
2020-02-11 23:04:56 +0800

    Update version to 2.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='memethesis',
-    version='2.1.1',
+    version='2.2.0',
     description='A CLI tool to visualize your memes',
     long_description=open('./README.md').read(),
     long_description_content_type='text/markdown',
