76d03be599b928b188878c5c1c8e067c7bf334fe
fakefred <fakefred@protonmail.ch>
2020-02-12 20:30:13 +0800

    Automate panel flag parsing in command mode

--- memethesis/__main__.py
+++ memethesis/__main__.py
@@ -15,6 +15,7 @@ FORMATS = read_formats()
 FMT_NAMES = get_format_names(FORMATS)
 MEMETHESIZERS = {k: MEMETHESIZERS_BY_FORMAT[v]
                  for k, v in get_compositions(FORMATS).items()}
+PANEL_TYPES = get_panel_types(FORMATS)
 
 
 def main():
@@ -43,33 +44,23 @@ def main():
         help='caption text to add above your meme'
     )
 
-    drake_args = argparser.add_argument_group('Drake')
-    drake_args.add_argument('--dislike', required=False)
-    drake_args.add_argument('--like', required=False)
+    # parse flags from formats.yml
+    for fk, fv in PANEL_TYPES.items():
+        # fk: format names
+        # fv.keys(): panel names
+        # fv.values(): panel flags
+        group = argparser.add_argument_group(fk)
+        for pv in fv:
+            group.add_argument('--' + str(pv))
 
-    brainsize_args = argparser.add_argument_group('Brain Size')
-    for i in range(1, 15):
-        brainsize_args.add_argument(
-            '-s{}'.format(i), '--size{}'.format(i), required=False
-        )
+    args = vars(argparser.parse_args())
 
-    womanyelling_args = argparser.add_argument_group('Woman Yelling')
-    womanyelling_args.add_argument('--woman', required=False)
-    womanyelling_args.add_argument('--cat', required=False)
-
-    pooh_args = argparser.add_argument_group('Winnie the Pooh')
-    pooh_args.add_argument('--tired', required=False)
-    pooh_args.add_argument('--wired', required=False)
-
-    args = argparser.parse_args()
-
-    if args.interactive:
+    if args['interactive']:
         interactive()
     else:
         # non-interactive (pure-cli) mode
-
         # validate format
-        if not args.format:
+        if not args['format']:
             print(color(
                 'Error: requires either -i/--interactive to be present or -f/--format specified.',
                 fgc=1))  # red
@@ -77,76 +68,38 @@ def main():
         # make sure the meme to be generated has something else to be
         # other than an ephemeral spectre in the volatile RAM,
         # like a file or some screen pixels
-        if not args.preview and not args.output:
+        if not args['preview'] and not args['output']:
             print(color(
                 'Error: either -p/--preview or -o/--output needs to be present.',
                 fgc=1))
             sys.exit(1)
 
-        if args.format == 'drake':
-            if args.dislike and args.like:
-                meme = make_vertical('drake', [
-                    ('dislike', args.dislike),
-                    ('like', args.like)
-                ])
-            else:
-                print(color(
-                    'Error: Drake memes require both --dislike and --like.',
-                    fgc=1))
-                sys.exit(1)  # show user an error right in their face
+        fmt = args['format']
+        memethesizer = MEMETHESIZERS[fmt]
 
-        elif args.format == 'brainsize':
-            brain_args = [args.size1, args.size2, args.size3, args.size4,
-                          args.size5, args.size6, args.size7,
-                          args.size8, args.size9, args.size10, args.size11,
-                          args.size12, args.size13, args.size14]  # lifehack!
-            if any(brain_args):
-                brains = []
-                for size, text in enumerate(brain_args, start=1):
-                    if text is not None:
-                        brains.append((size, text))
-                meme = make_vertical('brainsize', brains)
-            else:
-                print(color(
-                    'Error: Brain Size memes require at least one brain size',
-                    fgc=1))
-                sys.exit(1)
-        elif args.format == 'womanyelling':
-            if args.woman and args.cat:
-                meme = make_horizontal('womanyelling', [
-                    ('woman', args.woman),
-                    ('cat', args.cat)
-                ])
-            else:
-                print(color(
-                    'Error: Woman Yelling memes require both --woman and --cat.',
-                    fgc=1))
-                sys.exit(1)
-        elif args.format == 'pooh':
-            if args.tired and args.wired:
-                meme = make_vertical('pooh', [
-                    ('tired', args.tired),
-                    ('wired', args.wired)
-                ])
-            else:
-                print(color(
-                    'Error: Pooh memes requires both --tired and --wired.',
-                    fgc=1))
-                sys.exit(1)
+        meme = memethesizer(
+            # all the tuples with flags provided by formats.yml
+            # are added to the list as (flag, value)
+            # as a result, there are flags that are not populated.
+            # to eliminate such "blank" tuples,
+            # we run a filter against the list above.
+            fmt, filter(lambda tup: bool(tup[1]),
+                        [(n, args[n]) for n in PANEL_TYPES[fmt]]
+                        ))
 
-        if args.caption:
+        if args['caption']:
             meme = stack([
-                make_caption(text=args.caption, width=meme.size[0]),
+                make_caption(text=args['caption'], width=meme.size[0]),
                 make_sep(width=meme.size[0]),
                 meme
             ])
 
-        if args.preview:
+        if args['preview']:
             # uses PIL to preview the meme with ImageMagick
             meme.show(title='Memethesis Preview')
 
-        if args.output:
-            o = args.output
+        if args['output']:
+            o = args['output']
             path = ((o if search('\.(jpe?g|png)$', o, flags=I) else o + '.jpg')
                     if o else 'meme.jpg')
             meme.save(path)
